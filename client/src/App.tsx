acb91e3d61940628ac297a0719b9c14cb2fa79d1
markohautala <markohautala@live.se>
2024-09-09 20:14:14 +0200

    Add theme.ts with custom tailwind hues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
-function App() {
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react"
+import { themeSettings } from "./theme";
 
-  return (
-    <div className='app'>
-    </div>
-  )
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return <div className='app'>
+    <ThemeProvider theme={theme}> {/* applies the theme to the app */}
+      <CssBaseline />  {/* resets the default styles */}
+      <h1>Hi!</h1>
+    </ThemeProvider>
+  </div>;
 }
 
 export default App
