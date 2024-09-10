b56e67d39b790f0297f958d1675659c7f14a4fdf
markohautala <markohautala@live.se>
2024-09-10 22:17:42 +0200

    Add folders and start working on navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, dividerClasses, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react"
 import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className='app'>
-    <ThemeProvider theme={theme}> {/* applies the theme to the app */}
-      <CssBaseline />  {/* resets the default styles */}
-      <h1>Hi!</h1>
-    </ThemeProvider>
+    <BrowserRouter>  {/* enables routing */}
+      <ThemeProvider theme={theme}> {/* applies the theme to the app */}
+        <CssBaseline />  {/* resets the default styles */}
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<div>dashboard</div>} />
+            <Route path="/predictions" element={<div>predictions</div>} />
+          </Routes>
+        </Box> {/* the rest of the app goes here */}
+      </ThemeProvider>
+    </BrowserRouter>
   </div>;
 }
 
