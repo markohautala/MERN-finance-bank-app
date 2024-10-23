68ad4d29a7a49dd09e87031dc4a54c80878904b1
markohautala <markohautala@live.se>
2024-10-23 07:24:02 +0200

    Add KPI model and schemas for daily and monthly data

--- server/models/KPI.js
+++ server/models/KPI.js
@@ -4,6 +4,50 @@ import { loadType } from "mongoose-currency";
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
+const daySchema = new Schema(
+  {
+    date: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+  },
+  { toJSON: { getters: true } }
+)
+
+const monthSchema = new Schema(
+  {
+    month: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    operationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    nonOperationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+  },
+  { toJSON: { getters: true } }
+);
+
 const KPISchema = new Schema(
   {
     totalProfit: {
@@ -11,8 +55,29 @@ const KPISchema = new Schema(
       currency: "USD",
       get: (v) => v / 100
     },
-  }
-)
+    totalRevenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    totalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100
+    },
+    expenseByCategory: {
+      type: Map,
+      of: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100
+      }
+    },
+    monthlyData: [monthSchema],
+    dailyData: [daySchema],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
 
 const KPI = mongoose.model("KPI", KPISchema)
 
