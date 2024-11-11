b8b64d33b9713afadff272142a586814887a32cd
markohautala <markohautala@live.se>
2024-11-11 21:53:05 +0100

    Add and install recharts

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 type Props = {}
 
 const Row1 = (props: Props) => {
   const { data } = useGetKpisQuery();
+  console.log(data)
 
   return (
   <>
-    <DashboardBox  gridArea="a"></DashboardBox>
+    <DashboardBox  gridArea="a">
+    <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+        </AreaChart>
+      </ResponsiveContainer>
+    </DashboardBox>
     <DashboardBox  gridArea="b"></DashboardBox>
     <DashboardBox  gridArea="c"></DashboardBox>
   </>
