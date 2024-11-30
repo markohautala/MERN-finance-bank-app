05c2a6a1c4c6c361f1c7ec1b2021f526c7a59b05
markohautala <markohautala@live.se>
2024-11-30 11:37:12 +0100

    Add revenue bar chart

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -14,12 +14,26 @@ import {
   LineChart,
   CartesianGrid,
   Legend,
+  BarChart,
+  Bar,
+  Rectangle,
 } from "recharts";
 
 const Row1 = () => {
   const { palette } = useTheme();
   const { data } = useGetKpisQuery();
-  console.log(data);
+
+  const revenue = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+        };
+      })
+    );
+  }, [data]); // run only when data changes
 
   const revenueExpenses = useMemo(() => {
     return (
@@ -39,9 +53,9 @@ const Row1 = () => {
       data &&
       data[0].monthlyData.map(({ month, revenue, expenses }) => {
         return {
-          name: month.substring(0, 3),  // Only show the first 3 letters of the month
+          name: month.substring(0, 3), // Only show the first 3 letters of the month
           revenue: revenue,
-          profit: (revenue - expenses).toFixed(2),  // Calculate the profit. toFixed will round the number to 2 decimal places
+          profit: Number((revenue - expenses).toFixed(2)), // Calculate the profit. toFixed will round the number to 2 decimal places and convert it back to a number
         };
       })
     );
@@ -125,7 +139,7 @@ const Row1 = () => {
         </ResponsiveContainer>
       </DashboardBox>
       <DashboardBox gridArea="b">
-      <BoxHeader
+        <BoxHeader
           title="Profit and Revenue"
           subtitle="Top line represents revenue, bottom line represents expenses"
           sideText="+4%"
@@ -140,37 +154,41 @@ const Row1 = () => {
               bottom: 55, // bottom margin
             }}
           >
-            <CartesianGrid vertical={false} stroke={palette.grey[800]} />  // Only horisontal lines
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} /> //
+            Only horisontal lines
             <XAxis
               dataKey="name"
               tickLine={false}
               style={{ fontSize: "12px" }}
             />
             <YAxis
-              yAxisId="left"  // This is the axis on the left side
+              yAxisId="left" // This is the axis on the left side
               tickLine={false}
-              axisLine={false}  // This will remove the axis line
+              axisLine={false} // This will remove the axis line
               style={{ fontSize: "12px" }}
             />
             <YAxis
-              yAxisId="right"  // This is the axis on the right side
-              orientation="right"  // This will place the axis on the right side
+              yAxisId="right" // This is the axis on the right side
+              orientation="right" // This will place the axis on the right side
               tickLine={false}
               axisLine={false}
               style={{ fontSize: "12px" }}
             />
             <Tooltip />
-            <Legend height={20} wrapperStyle={{
-              margin: "0px 0px 10px 0px"
-            }}/>
+            <Legend
+              height={20}
+              wrapperStyle={{
+                margin: "0px 0px 10px 0px",
+              }}
+            />
             <Line
-              yAxisId="left"  // This will place the line on the left side
+              yAxisId="left" // This will place the line on the left side
               type="monotone"
               dataKey="profit"
               stroke={palette.tertiary[500]}
             />
             <Line
-              yAxisId="right"  // This will place the line on the right side
+              yAxisId="right" // This will place the line on the right side
               type="monotone"
               dataKey="revenue"
               stroke={palette.primary.main}
@@ -178,7 +196,59 @@ const Row1 = () => {
           </LineChart>
         </ResponsiveContainer>
       </DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="c">
+        <BoxHeader
+          title="Revenue by Month"
+          subtitle="Graph showing revenue by month"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={revenue}
+            margin={{
+              top: 17,
+              right: 15,
+              left: -5,
+              bottom: 58,
+            }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <Tooltip />
+            <Bar
+              dataKey="revenue"  // This is the key in the data object
+              fill="url(#colorRevenue)" // This is the gradient color
+              activeBar={<Rectangle fill="white" stroke="black" />}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </DashboardBox>
     </>
   );
 };
