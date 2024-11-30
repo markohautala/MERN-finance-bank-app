294a235d7d602acd8f442087a51941c94a4d4bb9
markohautala <markohautala@live.se>
2024-11-30 12:17:27 +0100

    Add product routes and integrate with the server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
 import { kpis } from "./data/data.js";
 
@@ -23,6 +25,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
