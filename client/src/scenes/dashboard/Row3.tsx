b22222e6edc0b881a7adb59bea7e84c56ee91dc5
markohautala <markohautala@live.se>
2024-12-07 22:38:55 +0100

    Add product list header and data grid to dashboard

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,5 +1,7 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from '@/state/api';
+import { DataGrid } from '@mui/x-data-grid';
 
 const Row3 = () => {
   const { data: kpiData } = useGetKpisQuery();
@@ -8,7 +10,13 @@ const Row3 = () => {
   console.log('transactionData', transactionData);
   return (
     <>
-      <DashboardBox  gridArea="g"></DashboardBox>
+      <DashboardBox  gridArea="g">
+        <BoxHeader
+          title='Product List'
+          sideText={`Products: ${productData?.length}`} />
+          <Box></Box>
+        <DataGrid />
+      </DashboardBox>
       <DashboardBox  gridArea="h"></DashboardBox>
       <DashboardBox  gridArea="i"></DashboardBox>
       <DashboardBox  gridArea="j"></DashboardBox>
