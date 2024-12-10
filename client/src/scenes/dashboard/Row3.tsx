2559a9b5157f58a57675cb6ae3075735744a9400
markohautala <markohautala@live.se>
2024-12-10 21:19:38 +0100

    Add transaction columns and enhance styling in dashboard

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -9,7 +9,33 @@ const Row3 = () => {
   const { data: kpiData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
   const { data: transactionData } = useGetTransactionsQuery();
+
   const productColumns = [
+    {
+      field: "_id",  // This is the key of the data
+      headerName: "ID",  // This is the header of the column
+      flex: 1,  // This column will take up 1 times the space of the other columns
+    },
+    {
+      field: "buyer",  // This is the key of the data
+      headerName: "Buyer",  // This is the header of the column
+      flex: 0.67,  // This column will take up 0.5 times the space of the other columns
+    },
+    {
+      field: "amount",  // This is the key of the data
+      headerName: "Amount",  // This is the header of the column
+      flex: 0.35,  // This column will take up 0.5 times the space of the other columns
+      renderCell: (params: GridCellParams) => `$${params.value}`,  // This is a function that will render the cell. In this case, it will add a $ sign in front of the value
+    },
+    {
+      field: "productIds",
+      headerName: "Count",
+      flex: 0.1,
+      renderCell: (params: GridCellParams) => params.value.length,
+    },
+  ];
+
+  const transactionColumns = [
     {
       field: "_id",  // This is the key of the data
       headerName: "ID",  // This is the header of the column
@@ -53,6 +79,14 @@ const Row3 = () => {
               "& .MuiDataGrid-columnSeparator": {
                 visibility: "hidden",
               },
+                // Change the color of the up/down arrows (sorting icons)
+              "& .MuiDataGrid-sortIcon": {
+                color: "white !important",  // This targets the sort icons (up and down arrows)
+              },
+              // Change the color of the three dots (more options)
+              "& .MuiIconButton-root": {
+                color: "white !important",  // This targets the three-dot icon and other buttons
+              },
             }}
           >
             <DataGrid
@@ -64,7 +98,47 @@ const Row3 = () => {
             />
           </Box>
       </DashboardBox>
-      <DashboardBox  gridArea="h"></DashboardBox>
+      <DashboardBox  gridArea="h">
+      <BoxHeader
+          title='Most Recent Transactions'
+          sideText={`${transactionData?.length} latest transactions`} />
+          <Box
+            mt="1rem"
+            p="0 0.5" // 0 padding on top and bottom, 0.5 padding on left and right
+            height="80%"
+            sx={{
+              "& .MuiDataGrid-root": {
+                color: palette.grey[300],
+                border: "none !important",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: `1px solid ${palette.grey[800]} !important`, // Remove the border at the bottom of each cell
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                borderBottom: `1px solid ${palette.grey[800]} !important`,
+              },
+              "& .MuiDataGrid-columnSeparator": {
+                visibility: "hidden",
+              },
+                // Change the color of the up/down arrows (sorting icons)
+              "& .MuiDataGrid-sortIcon": {
+                color: "white !important",  // This targets the sort icons (up and down arrows)
+              },
+              // Change the color of the three dots (more options)
+              "& .MuiIconButton-root": {
+                color: "white !important",  // This targets the three-dot icon and other buttons
+              },
+            }}
+          >
+            <DataGrid
+              columnHeaderHeight={25}  // This is the height of the header
+              rowHeight={30}  // This is the height of each row
+              hideFooter={true}  // This will hide the footer
+              rows={transactionData || []}  // This is where the data goes. || [] is a fallback in case productData is undefined
+              columns={transactionColumns}  // This is where the columns/vertical grids go
+            />
+          </Box>
+      </DashboardBox>
       <DashboardBox  gridArea="i"></DashboardBox>
       <DashboardBox  gridArea="j"></DashboardBox>
     </>
