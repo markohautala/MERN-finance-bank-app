55c1b4e6e11c0c5af11be812314f4aea15d1183c
markohautala <markohautala@live.se>
2024-12-26 22:17:22 +0100

    Add regression library and TypeScript definitions; configure ESLint and Babel

--- client/src/scenes/predictions/index.tsx
+++ client/src/scenes/predictions/index.tsx
@@ -1,32 +1,71 @@
-import { useGetKpisQuery } from '@/state/api';
-import { Box, Button, Typography, useTheme } from '@mui/material';
-import React, { useState } from 'react';
-import DashboardBox from '@/components/DashboardBox';
-import Flexbetween from '@/components/FlexBetween';
-import { CartesianGrid, Legend, Line, Tooltip, LineChart, ResponsiveContainer, XAxis, YAxis, Label } from 'recharts';
+import { useGetKpisQuery } from "@/state/api";
+import { Box, Button, Typography, useTheme } from "@mui/material";
+import { useMemo, useState } from "react";
+import DashboardBox from "@/components/DashboardBox";
+import Flexbetween from "@/components/FlexBetween";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  Tooltip,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Label,
+} from "recharts";
+import regression, { DataPoint } from "regression";
 
-type Props = {}
-
-const Predictions = (props: Props) => {
+const Predictions = () => {
   const { palette } = useTheme();
-  const { isPredictions, setIsPredictions } = useState(false);
+  const [isPredictions, setIsPredictions] = useState(false);
   const { data: kpiData } = useGetKpisQuery();
 
+  const formattedData = useMemo(() => {
+    if (!kpiData) return []; //So if kpiData does not exist, we want to return an empty arry
+    const monthData = kpiData[0].monthlyData; // Grabbing the monthly data from our kpiData
+
+    const formatted: Array<DataPoint> = monthData.map(
+      ({ revenue }, i: number) => {
+        return [i, revenue];
+      }
+    );
+    const regressionLine = regression.linear(formatted);
+
+    return monthData.map(({ month, revenue }, i: number) => {
+      return {
+        name: month,
+        "Actual Revenue": revenue,
+        "Regression Line": regressionLine.points[i][1],
+        "Predicted Revenue": regressionLine.predict(i + 12)[1],
+      };
+    });
+  }, [kpiData]);
+
   return (
-    <DashboardBox width="100%" height="100%" padding="1rem 2rem 4rem 2rem" overflow="hidden">
+    <DashboardBox
+      width="100%"
+      height="100%"
+      padding="1rem 2rem 4rem 2rem"
+      overflow="hidden"
+    >
       <Flexbetween m="1rem 2.5rem" gap="1rem">
         <Box>
           <Typography variant="h3">Revenue and Predictions</Typography>
-          <Typography variant="h6">Charted revenue and predicted revenue based on a linear regression model</Typography>
+          <Typography variant="h6">
+            Charted revenue and predicted revenue based on a linear regression
+            model
+          </Typography>
         </Box>
         <Button
-          onclick={() => setIsPredictions(!isPredictions)}
+          onClick={() => setIsPredictions(!isPredictions)}
           sx={{
             color: palette.grey[900],
             backgroundColor: palette.grey[500],
             boxShadow: "0.1rem 0.1rem 0.1rem 0.1rem rgba(0,0,0,0.2)",
           }}
-        >Show Predicted Revenue with Machine Learning Model
+        >
+          Show Predicted Revenue with Machine Learning Model
         </Button>
       </Flexbetween>
       <ResponsiveContainer width="100%" height="100%">
@@ -40,23 +79,24 @@ const Predictions = (props: Props) => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" stroke={palette.grey[800]} />
-          <XAxis
-            dataKey="name"
-            tickLine={false}
-            style={{ fontSize: "12px" }}
-          >
+          <XAxis dataKey="name" tickLine={false} style={{ fontSize: "12px" }}>
             <Label value="Month" offset={-5} position="insideBottom" />
           </XAxis>
           <YAxis
-            domain={["dataMin", "dataMax"]} // This will set the domain of the y-axis to the min and max of the data
+            domain={[12000, 26000]} // This will set the domain of the y-axis to the min and max of the data
             axisLine={{ strokeWidth: "0" }} // This will remove the axis line
             style={{ fontSize: "12px" }}
-            tickFormatter={(v) => `$${v / 1000}k`} // This will format the tick values to be in thousands
+            tickFormatter={(v) => `$${v}`} // This will format the tick values to be in thousands
           >
-            <Label value="Revenue" angle={-90} offset={-5} position="insideLeft" />
+            <Label
+              value="Revenue"
+              angle={-90}
+              offset={-5}
+              position="insideLeft"
+            />
           </YAxis>
           <Tooltip />
-          <Legend verticalAlign="top"/>
+          <Legend verticalAlign="top" />
           <Line
             type="monotone"
             dataKey="Actual Revenue"
@@ -72,15 +112,15 @@ const Predictions = (props: Props) => {
           />
           {isPredictions && (
             <Line
-              type="monotone"
-              dataKey="Regression Line"
-              stroke="#8884d8"
+              strokeDasharray="5 5"
+              dataKey="Predicted Revenue"
+              stroke={palette.secondary[500]}
             />
           )}
         </LineChart>
       </ResponsiveContainer>
     </DashboardBox>
-  )
-}
+  );
+};
 
-export default Predictions
+export default Predictions;
