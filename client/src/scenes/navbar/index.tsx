93d4df4cff8bca3bb8c2d09ae933c11fa1e1c105
markohautala <markohautala@live.se>
2024-12-28 11:32:22 +0100

    Refactor Navbar component to remove unused props

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -4,9 +4,7 @@ import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from "@/components/FlexBetween";
 import AccountBalanceSharpIcon from "@mui/icons-material/AccountBalanceSharp";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard"); // State to keep track of selected navigation item
 
