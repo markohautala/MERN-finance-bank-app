089e453954034093584850d9342f511fbdf558af
markohautala <markohautala@live.se>
2024-12-28 10:51:09 +0100

    Fixed TypeScript unused import issue in theme augmentation

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,15 +1,10 @@
-// eslint-disable-next-line
-import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
-
 // Extending the default MUI Palette types to include a 'tertiary' color
 declare module "@mui/material/styles/createPalette" {
-  // Adding support for numeric keys (like 100, 200, etc.) in the PaletteColor interface
   interface PaletteColor {
     [key: number]: string;
   }
 
-  // Adding a new 'tertiary' color to the Palette interface
   interface Palette {
-    tertiary: PaletteColor;  // 'tertiary' is now part of the palette and will be of type PaletteColor
+    tertiary: PaletteColor;
   }
 }
